e9cf3259a851f5a8a6b2c83c6cc82932c8bd637c
Bruno Arsieli <149199321+brunoarsieli@users.noreply.github.com>
2023-12-22 18:42:24 -0300

    primeiro commit

---
+++ temporizador.py
@@ -0,0 +1,13 @@
+from winsound import PlaySound, SND_FILENAME
+from time import sleep
+tempo = 1501
+horas = 0
+for t in range(1, 1501):
+    tempo -= 1
+    minutos = tempo/60
+    if minutos <= 60:
+        print(f"Falta {horas} horas e {minutos:.2f} minutos")
+    else:
+        pass
+    sleep(1)
+PlaySound('tone1.wav', SND_FILENAME) * 3
